7f572926718bacc00abe60a5d49dc163c428b34d
Jenn Marshall <jennyfever@gmail.com>
2022-11-29 17:05:47 -0700

    edited readfile in notesrouter, notes appearing now

--- routesjs/notesRouter.js
+++ routesjs/notesRouter.js
@@ -6,6 +6,33 @@ const newId = require('../helpers/newId')
 // allows for modular routing
 const api = require('express').Router();
 
+// parses the json file to be read
+api.get('/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+
+    res.json(notes);
+});
+
+// newid works, is adding to db, is not posting to 
+api.post('/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: newId()
+    }
+
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes), (err, res) => {
+        if (err) throw err;
+        return true;
+    });
+
+    res.json(notes);
+})
+
+module.exports = api;
+
 
 
 
